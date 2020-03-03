88fe589179959deacfc619961e2733d284e53d5f
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-03-02 16:27:05 -0800

    add styles to header menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,41 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import logo from '../media/logo.svg';
+
 const Wrapper = styled.div `
   display: flex;
   flex-direction: column;
-  border: 1px solid yellow;
   width: 275px;
+`
+
+const Logo = styled.img `
+  width: 50px;
+  padding: 0 20px;
 `
 
 const MenuItem = styled.a `
   text-decoration: none;
   color: white;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 10px 20px;
+  margin-top: 10px;
+  width: fit-content;
+  border-radius: 999px;
+  transition: background-color .2s;
+  transition: color .2s;
+
+  &:hover {
+    background-color: #20303D;
+    color: rgb(29, 161, 242);
+  }
 `
 
 function Header () {
   return (
     <Wrapper>
+      <Logo src={logo} alt="logo"/>
       <MenuItem href="/">Home</MenuItem>
       <MenuItem href="/">Explore</MenuItem>
       <MenuItem href="/">Notifications</MenuItem>
