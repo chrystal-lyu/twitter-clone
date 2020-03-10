e05cf6a5b744043cfb6db280dafa36c8844dacaa
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-03-09 17:03:51 -0700

    add search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,83 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
+import sentences from '../sentences.json'
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
+const SearchBox = styled.input`
+  margin: 15px;
+  padding: 10px;
+  background: rgb(37,51,65);
+  color: white;
+  height: 30px;
+  width: 80%;
+  font-size: 16px;
+  border: none;
+
+  &:focus {
+    outline: none;
+  }
+`
+const SearchResult = styled.div`
+  border-top: 1px solid rgb(56,68,77);
 `
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      searchValue: '',
+      searchResult: []
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      searchResult: sentences
+    })
+  }
+
+  handleChange (e) {
+    const target = e.target.value;
+    let filteredResult = [];
+    filteredResult = sentences.filter((item) => {
+      let text = item.data.toLowerCase();
+      return text.indexOf(target.toLowerCase()) > -1;
+    })
+    this.setState({
+      searchValue: target,
+      searchResult: filteredResult
+    })
+  }
+
   render() {
+    const { searchValue, searchResult} = this.state;
     return (
-      <Wrapper>This is search</Wrapper>
+      <Wrapper>
+        <SearchBox
+          name = 'text'
+          type = 'text'
+          placeholder = 'What are you looking for?'
+          value = {searchValue}
+          onChange = {this.handleChange}
+        />
+        <SearchResult>
+          <ul>
+            {searchResult.map((item) => {
+              return (
+                <li key={item.id}>{item.data}</li>
+              )
+            })}
+          </ul>
+        </SearchResult>
+      </Wrapper>
     )
   }
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Search from './Search';
 
 const Wrapper = styled.div `
-  border: 1px solid lightblue;
+  /* border: 1px solid lightblue; */
   width: 350px;
 `
 
