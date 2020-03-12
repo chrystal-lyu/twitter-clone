1dd8f1319414b5245aceb661716f35f216cc180a
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-03-12 15:32:04 -0700

    add timeline loader animation

--- src/components/FeedList.jsx
+++ src/components/FeedList.jsx
@@ -5,6 +5,24 @@ import FeedItem from './FeedItem';
 const Wrapper = styled.div `
   /* padding: 0 20px; */
 `
+const LoaderWrapper = styled.div `
+  margin-top: 20px;
+`
+const Loader = styled.div`
+  border: 10px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 10px solid rgb(29,161,242);
+  width: 20px;
+  height: 20px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+  margin: 0 auto;
+
+  @keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+  }
+`
 const List = styled.ul`
   display: flex;
   flex-direction: column-reverse;
@@ -16,9 +34,9 @@ class FeedList extends React.Component {
   render () {
     if (this.props.data.length === 0) {
       return (
-        <Wrapper>
-            Be the first one to tweet!
-        </Wrapper>
+        <LoaderWrapper>
+            <Loader/>
+        </LoaderWrapper>
       )
     } else {
       return (
