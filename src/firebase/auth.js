4d086c8816941ce00d3dae84729814e69520b9ed
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-03-27 11:38:59 -0700

    clean up firebase config setting

--- src/firebase/auth.js
+++
@@ -1,9 +0,0 @@
-import firebase from './firebase';
-
-export const getAuth = () => {
-  return firebase.auth();
-};
-
-export const twitterOAuth = () => {
-  return new firebase.firebase_.auth.TwitterAuthProvider();
-};
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,14 @@
-import firebase from './firebase';
-import * as auth from './auth';
+import firebase from 'firebase';
 
-export { firebase, auth };
+try {
+  const config = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
+  };
+  firebase.initializeApp(config);
+} catch (err) {
+  console.log(err);
+}
+
+export const twitterProvider = new firebase.auth.TwitterAuthProvider();
+export default firebase;
