52b52b87aab99df44413af1297ff5aa263dde478
Chrystal Lyu <chrystal.lyu@gmail.com>
2020-04-09 14:52:39 -0700

    fix express get domain

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const client = new Twitter ({
 
 app.use(express.static(path.join(__dirname + '/build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
